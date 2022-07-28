c33b21f70a3a31f529eb7303a7b6d6015a4367ff
carl <carl.hickton@gmail.com>
2022-07-28 18:22:05 +1000

    chore: updated logging

--- libs/listner/src/utils/logging.ts
+++ libs/listner/src/utils/logging.ts
@@ -5,11 +5,14 @@ const l = (v: string, ...parms: any[]) => console.log(v, ...parms);
 const info = () => `[${new Date().toLocaleString()}]`;
 
 export const logging = {
-  error: (msg: string, ...parms: any[]) => l(chalk.red(info(), msg), ...parms),
+  error: (msg: string, ...parms: any[]) =>
+    l(chalk.red(info(), 'ERROR:', msg), ...parms),
   warn: (msg: string, ...parms: any[]) =>
-    l(chalk.yellow(info(), msg), ...parms),
-  log: (msg: string, ...parms: any[]) => l(chalk.green(info(), msg), ...parms),
-  info: (msg: string, ...parms: any[]) => l(chalk.gray(info(), msg), ...parms),
+    l(chalk.yellow(info(), 'WARNING:', msg), ...parms),
+  log: (msg: string, ...parms: any[]) =>
+    l(chalk.green(info(), 'LOG:', msg), ...parms),
+  info: (msg: string, ...parms: any[]) =>
+    l(chalk.gray(info(), 'INFO:', msg), ...parms),
   debug: (msg: string, ...parms: any[]) =>
-    l(chalk.white(info(), msg), ...parms),
+    l(chalk.white(info(), 'DEBUG:', msg), ...parms),
 };
