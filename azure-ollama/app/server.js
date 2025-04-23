45b57acda9c4fd55f1c2cecacbc0f026d6cd3abc
carl <carl.hickton@gmail.com>
2025-04-23 13:34:00 +1000

    chore: getting working

--- azure-ollama/app/server.js
+++ azure-ollama/app/server.js
@@ -7,6 +7,10 @@ import { StringOutputParser } from '@langchain/core/output_parsers';
 import { ChatPromptTemplate } from '@langchain/core/prompts';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const config = getConfig();
 // Use the port from config if available, otherwise default
 const port = config.port || 8099;
@@ -47,13 +51,17 @@ app.get('/', async (req, res) => {
   res.send(`${message} ${result}`);
 });
 
-app.post('/', async (req, res) => {
+app.get('/api/tags', (req, res) => {
+  logInfo('Received request on /api/tags');
+  res.send(`'tag1', 'tag2', 'tag3'`);
+});
+
+app.post('/api/chat', async (req, res) => {
   logInfo('Received POST request on /');
 
-  
-
-
   console.log('Request body:', req.body); // Log the request body
+
+  const { body } = req;
 
   //   body = {
   //     "model": model,
@@ -62,29 +70,37 @@ app.post('/', async (req, res) => {
   //     "options": {"temperature": DEFAULT_TEMPERATURE, "num_predict": max_tok},
   // }
 
-  const c = config.logins.at(0);
+  const c =
+    config.logins.find((x) => x.model === body.model) || config.logins.at(0);
+
+  logInfo('Using model:', c.model);
 
   const model = new AzureChatOpenAI({
-    temperature: 0.9,
+    temperature: body.options.temperature || 0.9,
     azureOpenAIApiKey: c.apikey,
     azureOpenAIEndpoint: c.endpoint,
     azureOpenAIApiDeploymentName: c.model,
     azureOpenAIApiVersion: config.apiVersion,
   });
 
-  const prompt = ChatPromptTemplate.fromTemplate(
-    'tell me a joke about {topic}'
-  );
+  const prompt = ChatPromptTemplate.fromTemplate('{input}');
 
   const chain = prompt.pipe(model).pipe(new StringOutputParser());
 
-  const result = await chain.invoke({ topic: 'cats' });
+  const result = await chain.invoke({ input: body.messages.at(0).content });
+
+  console.log('Result:', result); // Log the result
 
   res.send({
-    massage: {
+    message: {
       content: result,
     },
   });
+});
+
+app.use((req, res, next) => {
+  logWarning(`404 Not Found: ${req.method} ${req.originalUrl}`);
+  res.status(404).send("Sorry, can't find that!");
 });
 
 logInfo('Node.js application starting...');
