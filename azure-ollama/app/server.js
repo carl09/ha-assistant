8d705ac78f54f833cec96263f1fd8203235d0590
carl <carl.hickton@gmail.com>
2025-04-23 14:08:46 +1000

    chore: working on chat

--- azure-ollama/app/server.js
+++ azure-ollama/app/server.js
@@ -1,10 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
-import { logInfo, logWarning } from './logging.js';
+import { logError, logInfo, logWarning } from './logging.js';
 import { getConfig } from './config.js';
 import { AzureChatOpenAI } from '@langchain/openai';
 import { StringOutputParser } from '@langchain/core/output_parsers';
 import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { HumanMessage, SystemMessage } from '@langchain/core/messages';
 
 const app = express();
 
@@ -21,39 +22,24 @@ if (!process.env.CONFIG_MESSAGE) {
   logWarning('CONFIG_MESSAGE environment variable not set, using default.');
 }
 
-app.get('/', async (req, res) => {
-  logInfo('Received request on /');
-
-  console.log('Request headers:', req.headers); // Log the request headers
-  console.log('Request method:', req.method); // Log the request method
-  console.log('Request URL:', req.url); // Log the request URL
-  console.log('Request query:', req.query); // Log the request query parameters
-
-  const c = config.logins.at(0);
-
-  const model = new AzureChatOpenAI({
-    temperature: 0.9,
-    azureOpenAIApiKey: c.apikey,
-    azureOpenAIEndpoint: c.endpoint,
-    azureOpenAIApiDeploymentName: c.model,
-    azureOpenAIApiVersion: config.apiVersion,
-  });
-
-  const prompt = ChatPromptTemplate.fromTemplate(
-    'tell me a joke about {topic}'
-  );
-
-  const chain = prompt.pipe(model).pipe(new StringOutputParser());
-
-  const result = await chain.invoke({ topic: 'cats' });
-  logInfo(`Result: ${result}`);
-
-  res.send(`${message} ${result}`);
-});
-
 app.get('/api/tags', (req, res) => {
   logInfo('Received request on /api/tags');
-  res.send(`'tag1', 'tag2', 'tag3'`);
+
+  res.send({
+    models: config.logins.map((x) => ({
+      name: x.model,
+      model: x.model,
+    })),
+  });
+
+  // res.send({
+  //   models: [
+  //     {
+  //       name: 'gpt-4o',
+  //       model: 'gpt-4o',
+  //     },
+  //   ],
+  // });
 });
 
 app.post('/api/chat', async (req, res) => {
@@ -63,17 +49,24 @@ app.post('/api/chat', async (req, res) => {
 
   const { body } = req;
 
-  //   body = {
-  //     "model": model,
-  //     "messages": [{"role": "user", "content": prompt}],
-  //     "stream": False,
-  //     "options": {"temperature": DEFAULT_TEMPERATURE, "num_predict": max_tok},
-  // }
+  const messages = [];
+
+  for (const message of body.messages) {
+    if (message.role === 'user') {
+      messages.push(new HumanMessage(message.content));
+    } else if (message.role === 'assistant') {
+    } else if (message.role === 'system') {
+      messages.push(new SystemMessage(message.content));
+    } else {
+      logWarning(`Unknown role: ${message.role}`);
+      continue;
+    }
+  }
 
   const c =
     config.logins.find((x) => x.model === body.model) || config.logins.at(0);
 
-  logInfo('Using model:', c.model);
+  logInfo(`Using model: ${c.model}`);
 
   const model = new AzureChatOpenAI({
     temperature: body.options.temperature || 0.9,
@@ -83,23 +76,24 @@ app.post('/api/chat', async (req, res) => {
     azureOpenAIApiVersion: config.apiVersion,
   });
 
-  const prompt = ChatPromptTemplate.fromTemplate('{input}');
+  const chain = model.pipe(new StringOutputParser());
 
-  const chain = prompt.pipe(model).pipe(new StringOutputParser());
+  const result = await chain.invoke(messages);
 
-  const result = await chain.invoke({ input: body.messages.at(0).content });
-
-  console.log('Result:', result); // Log the result
+  console.log('Result:', result);
 
   res.send({
+    model: c.model,
+    created_at: new Date().toISOString(),
     message: {
+      role: 'assistant',
       content: result,
     },
   });
 });
 
 app.use((req, res, next) => {
-  logWarning(`404 Not Found: ${req.method} ${req.originalUrl}`);
+  logError(`404 Not Found: ${req.method} ${req.originalUrl}`);
   res.status(404).send("Sorry, can't find that!");
 });
 
